ca126631bd8b951e49bbc478b7a116b457a302fa
nikkaramessinis <nikkaramessinis@github.com>
2024-05-21 20:36:17 +0100

    backtesting and yfinance_attempt

---
+++ backtesting_sma.py
@@ -0,0 +1,63 @@
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+
+from backtesting.test import SMA, MACD, GOOG
+
+
+class SmaCross(Strategy):
+    def init(self):
+        price = self.data.Close
+        #self.ma1 = self.I(SMA, price, 5)
+        self.ma1 = self.I(SMA, price, 10)
+        self.ma2 = self.I(SMA, price, 25)
+
+
+
+
+    def next(self):
+        if crossover(self.ma1, self.ma2):# or crossover(self.ma2, self.ma3):
+            self.buy()
+        elif crossover(self.ma2, self.ma1):# and crossover(self.ma3, self.ma2):
+            self.sell()
+
+
+import pandas as pd
+from backtesting.lib import SignalStrategy, TrailingStrategy
+
+
+class SmaCross1(SignalStrategy,
+               TrailingStrategy):
+    n1 = 10
+    n2 = 25
+
+    def init(self):
+        # In init() and in next() it is important to call the
+        # super method to properly initialize the parent classes
+        super().init()
+
+        # Precompute the two moving averages
+        sma1 = self.I(MACD, self.data.Close, self.n1)
+        sma2 = self.I(MACD, self.data.Close, self.n2)
+
+        # Where sma1 crosses sma2 upwards. Diff gives us [-1,0, *1*]
+        signal = (pd.Series(sma1) > sma2).astype(int).diff().fillna(0)
+        signal = signal.replace(-1, 0)  # Upwards/long only
+
+        # Use 95% of available liquidity (at the time) on each order.
+        # (Leaving a value of 1. would instead buy a single share.)
+        entry_size = signal * .95
+
+        # Set order entry sizes using the method provided by
+        # `SignalStrategy`. See the docs.
+        self.set_signal(entry_size=entry_size)
+
+        # Set trailing stop-loss to 2x ATR using
+        # the method provided by `TrailingStrategy`
+        self.set_trailing_sl(2)
+
+
+bt = Backtest(GOOG, SmaCross1, commission=.002,
+              exclusive_orders=True)
+stats = bt.run()
+print(stats)
+bt.plot()
--- iex.py
+++ iex.py
@@ -1,11 +1,12 @@
-import numpy as np #The Numpy numerical computing library
+import numpy as np
 import pandas as pd #The Pandas data science library
 import requests #The requests library for HTTP requests in Python
 import xlsxwriter #The XlsxWriter libarary for
 import math #The Python math module
 from scipy import stats #The SciPy stats module
 import bs4 as bs
-from secrets import  IEX_CLOUD_API_TOKEN
+from mysecrets import  IEX_CLOUD_API_TOKEN
+import certifi
 
 def retrieve_sp500_tickers() -> list[str]:
     # Visit the Wikipedia page
@@ -40,18 +41,27 @@ rv_columns = [
     'EV/EBITDA Percentile',
     'EV/GP',
     'EV/GP Percentile',
-    'RV Score'
+    'RV Score',
+    'net_income',
+    'equity',
+    'revenue',
+    'eps'
 ]
 
+def get_fundamentals(symbol_string):
+    url = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote,advanced-stats,cash-flow,fundamentals&token={IEX_CLOUD_API_TOKEN}'
+    print(url)
+
+    response = requests.get(url, verify=certifi.where())
+    data = response.json()
+    return data
 
 
 def get_initial_dataframe(sp500_symbols):
     rv_dataframe = pd.DataFrame()
     new_rows = []
     for symbol_string in sp500_symbols:
-        batch_api_call_url = f'https://cloud.iexapis.com/stable/stock/market/batch?symbols={symbol_string}&types=quote,advanced-stats&token={IEX_CLOUD_API_TOKEN}'
-        print(batch_api_call_url)
-        data = requests.get(batch_api_call_url).json()
+        data = get_fundamentals(symbol_string)
         for symbol in symbol_string.split(','):
             enterprise_value = data[symbol]['advanced-stats']['enterpriseValue']
             ebitda = data[symbol]['advanced-stats']['EBITDA']
@@ -67,6 +77,7 @@ def get_initial_dataframe(sp500_symbols):
             except TypeError:
                 ev_to_gross_profit = np.NaN
 
+
             new_row = [
                     symbol,
                     data[symbol]['quote']['latestPrice'],
@@ -81,8 +92,12 @@ def get_initial_dataframe(sp500_symbols):
                     'N/A',
                     ev_to_gross_profit,
                     'N/A',
-                    'N/A'
-                ]
+                    'N/A',
+                    data[symbol].get('cash-flow', {}).get('cashflow', [{}])[0].get('netIncome', float('nan')),
+                    data[symbol].get('fundamentals', {}).get('fundamentals', [{}])[0].get('equityShareholder', float('nan')),
+                    data[symbol].get('fundamentals', {}).get('fundamentals', [{}])[0].get('revenue', float('nan')),
+                    data[symbol].get('fundamentals', {}).get('fundamentals', [{}])[0].get('incomeNetPerWabsoSplitAdjusted', float('nan'))
+            ]
             new_rows.append(new_row)
 
     # Convert the list of lists to a DataFrame
@@ -91,11 +106,66 @@ def get_initial_dataframe(sp500_symbols):
     # Concatenate the new rows to the existing DataFrame
     rv_dataframe = pd.concat([rv_dataframe, new_rows_df], ignore_index=True)
 
+
     # Save the result to a CSV file
-    rv_dataframe.to_csv("rv_dataframe.csv", index=False)
+    #rv_dataframe.to_csv("nikola.csv", index=False)
     return rv_dataframe
+
+
+def assign_composite_score(df):
+    pe_ratio = df["Price-to-Earnings Ratio"].astype(float)
+    pb_ratio = df["Price-to-Book Ratio"].astype(float)
+    print(pb_ratio)
+    #free_cash_flow = df["Free Cash Flow"]
+    normalized_pe = pe_ratio / max(pe_ratio.max(), pb_ratio.max())
+    normalized_pb = pb_ratio / max(pe_ratio.max(), pb_ratio.max())
+    #normalized_fcf = free_cash_flow / max(pe_ratio, pb_ratio, free_cash_flow)
+
+    weight_pe = 0.6
+    weight_pb = 0.4
+    weight_fcf = 0
+
+    # Calculate the composite score
+    composite_score = (weight_pe * normalized_pe) + (weight_pb * normalized_pb)# + (weight_fcf * normalized_fcf)
+    df = df.assign(composite_score=composite_score)
+    return df
+
+
+def min_max_normalize(value, min_val, max_val):
+    return (value - min_val) / (max_val - min_val)
+
+def assign_financial_composite_metric(df):
+    net_income = df.get('net_income').astype(float)
+    equity = df.get('equity').astype(float)
+    revenue = df.get('revenue').astype(float)
+    eps = df.get('eps').astype(float)
+    stock_price = df.get('Price').astype(float)  # Replace with actual stock price
+
+    roe = net_income / equity
+    profit_margin = net_income / revenue
+    earnings_yield = eps / stock_price
+
+    # Normalize the ratios (optional)
+    # Min-Max normalize each ratio
+    roe_normalized = min_max_normalize(roe, min(roe), max(roe))
+    profit_margin_normalized = min_max_normalize(profit_margin, min(profit_margin), max(profit_margin))
+    earnings_yield_normalized = min_max_normalize(earnings_yield, min(earnings_yield), max(earnings_yield))
+
+    # Calculate the composite metric (e.g., average)
+    composite_financial_metric = (roe_normalized + profit_margin_normalized + earnings_yield_normalized) / 3
+    df = df.assign(composite_financial_score=composite_financial_metric)
+    return df
 
 
 if __name__ == "__main__":
     sp500_symbols = retrieve_sp500_tickers()
-    rv_dataframe = get_initial_dataframe(sp500_symbols)
+    #sp500_symbols = ["MMM", "AAPL"]
+    #sp500_symbols = ["NKLA"]
+    # either query the dataframe online or read from the existing csv
+    #rv_dataframe = get_initial_dataframe(sp500_symbols)
+    rv_dataframe = pd.read_csv("sp500.csv")
+
+    rv_dataframe = assign_composite_score(rv_dataframe)
+    rv_dataframe = assign_financial_composite_metric(rv_dataframe)
+    rv_dataframe = rv_dataframe.sort_values(by='composite_score', ascending=False)
+    rv_dataframe.to_csv("sthsth_new.csv", index=False)
