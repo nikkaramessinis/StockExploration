7171c72c42174acade89288f42bf0f58b0f93426
vivek <viveksdchauhan@gmail.com>
2024-06-22 15:31:19 +0100

    fix the calculation of the cell colors

--- config/feqos.py
+++ config/feqos.py
@@ -1,53 +1,96 @@
 import pandas as pd
 
+should_reverse_obj = {
+    "Exposure Time [%]": True,
+    "Equity Final [$]": False,
+    "Equity Peak [$]": False,
+    "Return [%]": False,
+    "Buy & Hold Return [%]": False,
+    "Return (Ann.) [%]": False,
+    "Volatility (Ann.) [%]": True,
+    "Sharpe Ratio": False,
+    "Sortino Ratio": False,
+    "Calmar Ratio": "DROP",
+    "Max. Drawdown [%]": True,
+    "Avg. Drawdown [%]": True,
+    "Max. Drawdown Duration": True,
+    "Avg. Drawdown Duration": True,
+    "# Trades": True,
+    "Win Rate [%]": False,
+    "Best Trade [%]": False,
+    "Worst Trade [%]": True,
+    "Avg. Trade [%]": False,
+    "Max. Trade Duration": True,
+    "Avg. Trade Duration": True,
+    "Profit Factor": False,
+    "Expectancy [%]": "DROP",
+    "SQN": False,
+    "_strategy": "",
+    "Name": "",
+    "source": "",
+}
 
-def get_cell_color(val, ref):
+
+def get_cell_color(val, ref, reverse=False):
+    positive_color = "green"
+    negative_color = "red"
+    if reverse:
+        positive_color = "red"
+        negative_color = "green"
+
     if (
         isinstance(val, float)
         and not isinstance(val, pd.Timestamp)
         and (isinstance(val, float) and not isinstance(ref, str))
     ):
-        print(f"Val {val} Ref {ref} Val {type(val)} Ref {type(ref)}")
         if val > ref:
-            return "green"
+            return positive_color
         elif val < ref:
-            return "red"
-    else:
-        print(f"Val {val} Ref {ref} Val {type(val)} Ref {type(ref)}")
-    return "yellow"
+            return negative_color
+    return ""
 
 
 def color_all_cells(df_me):
     df_colored = df_me.copy()
     colours = []
     for index, row in df_colored.iterrows():
-        inner_colors = ["yellow"] * len(df_colored.columns)  # Initialize with "yellow"
-        print(f"len(inner_colors){len(inner_colors)}")
-        print(f"len(columns){len(df_colored.columns)}")
+        inner_colors = ["yellow"] * len(df_colored.columns)
 
         if row["source"] == "Test":
             filtered_row = df_colored[
                 (df_colored["Name"] == row["Name"])
                 & (df_colored["source"] == "Reference")
             ]
+
             for col in df_colored.columns:
-                cell_value = row[col]
-                color = get_cell_color(cell_value, filtered_row[col].iloc[0])
-                inner_colors[df_colored.columns.get_loc(col)] = color
+                if col in should_reverse_obj and should_reverse_obj[col] != "DROP":
+                    cell_value = row[col]
+                    color = get_cell_color(
+                        cell_value,
+                        filtered_row[col].iloc[0],
+                        should_reverse_obj[col],
+                    )
+                    inner_colors[df_colored.columns.get_loc(col)] = color
+
         colours.append(inner_colors)
     return colours
 
 
 def create_colored_html_table(df_me):
+    # Drop columns marked as "DROP"
+    cols_to_drop = [
+        col for col, action in should_reverse_obj.items() if action == "DROP"
+    ]
+
+    df_me = df_me.drop(columns=cols_to_drop)
+
     colors = color_all_cells(df_me)
 
-    # Convert DataFrame to HTML with styles
     def apply_colors_to_html(row, colors):
         return ["background-color: {}".format(color) for color in colors[row.name]]
 
     styled_df = df_me.style.apply(apply_colors_to_html, colors=colors, axis=1)
 
-    # Save the table as an HTML file
     html_table = styled_df._repr_html_()
     with open("colored_table.html", "w") as f:
         f.write(html_table)
@@ -60,7 +103,6 @@ def merge_reference_with_test(df_test):
     df_reference["source"] = "Reference"
     df_test["source"] = "Test"
 
-    # Define a function to apply colors based on the comparison
     df_me = pd.concat([df_reference, df_test], ignore_index=True)
     df_me = df_me.sort_values(by=["Name", "source"])
     df_me.drop("_trades", axis=1, inplace=True)
