95b34bc07d4c3b25580aad9e52f8270e698a1592
nikkaramessinis <nikkaramessinis@github.com>
2024-06-30 21:52:21 +0100

    Adding incomplete mix strategy and refactoring optimizers.
    I have to delete the multiple optimizers.

--- optimizers/optimizer.py
+++ optimizers/optimizer.py
@@ -1,11 +1,52 @@
 from abc import ABC, abstractmethod
-from typing import Dict, Any
+from collections import defaultdict
+from typing import Optional, Callable, Dict, Any, Union, Tuple
+from backtesting import Backtest
+from utils.helpers import fetch_latest_data, fill_with_ta
+
 
 class Optimizer:
-    def __init__(self, strategy, stocks_list):
+    def __init__(self, strategy, stocks_list, default_values: Dict):
         self.strategy = strategy
         self.stocks_list = stocks_list
+        self.default_values = default_values
 
-    @abstractmethod
-    def optimize(self) -> Dict[str, Any]:
-        pass
+    @staticmethod
+    def optimize_strategy(bt,
+        param_ranges: Dict[str, Union[range, list]],
+        constraint_func: Optional[Callable[[Any], bool]],
+        maximize_metric: str = "Equity Final [$]"
+    ) -> Tuple[Any, Any]:
+
+        stats, optiheatmap = bt.optimize(
+            **param_ranges,
+            maximize=maximize_metric,
+            constraint=constraint_func,
+            return_heatmap=True
+        )
+        return stats, optiheatmap
+
+    def optimize(self, param_ranges, constraint_func: Optional[Callable[[Any], bool]]=None) -> Dict[str, Any]:
+        param_sums = defaultdict(int)
+        for symbol in self.stocks_list:
+            print(f"Calling fetching data for symbol {symbol}")
+            df = fetch_latest_data(symbol)
+            df = fill_with_ta(df)
+            bt = Backtest(df, self.strategy, cash=1000, commission=0.002, exclusive_orders=True)
+            stats, optiheatmap = Optimizer.optimize_strategy(bt, param_ranges, constraint_func)
+            best_params = optiheatmap.idxmax(skipna=True)
+            if isinstance(best_params, tuple):
+                for param, value in zip(param_ranges.keys(), best_params):
+                    param_sums[param] += value
+            else:
+                for param, default_value in self.default_values.items():
+                    param_sums[param] += default_value
+                pass
+
+            stats['Name'] = symbol
+        
+        # Calculate averages for each parameter
+        averaged_params = {param: value // len(self.stocks_list) for param, value in param_sums.items()}
+        
+        print(f"Averaged parameters: {averaged_params}")
+        return averaged_params
---
+++ optimizers/strategy_params.py
@@ -0,0 +1,36 @@
+class StrategyParams(object):
+    def __init__(self, param_ranges, param_defaults):
+        self.param_ranges = param_ranges
+        self.param_defaults = param_defaults
+
+
+strategy_params = {
+    "RSI": StrategyParams(
+        param_ranges={
+            "upper_bound": range(40, 85, 5),
+            "lower_bound": range(10, 45, 5),
+            "rsi_window": range(10, 30, 2),
+        },
+        param_defaults={
+            "upper_bound": 70,
+            "lower_bound": 30,
+            "rsi_window": 15,
+        },
+    )
+}
+
+
+def get_params_ranges(strategy_name):
+    if strategy_name not in strategy_params:
+        assert (
+            False
+        ), f"Strategy_name {strategy_name} does not exist in strategy_params!"
+    return (strategy_params[strategy_name]).param_ranges
+
+
+def get_params_defaults(strategy_name):
+    if strategy_name not in strategy_params:
+        assert (
+            False
+        ), f"Strategy_name {strategy_name} does not exist in strategy_params!"
+    return strategy_params[strategy_name].param_defaults
