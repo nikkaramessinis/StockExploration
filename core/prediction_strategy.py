be51101f56bef9aa007280ff0f20742db1c0de8c
Vivek <viveksdchauhan@gmail.com>
2024-07-07 09:34:01 +0100

    fix conflicts and venv env

--- core/prediction_strategy.py
+++ core/prediction_strategy.py
@@ -1,13 +1,9 @@
-from optimizers.optimizer import Optimizer
-from optimizers.strategy_params import get_params_defaults, get_params_ranges
-from strategies.backtesting_bollinger_bands import BBandsCross
 from strategies.backtesting_ema import EmaCross
-from strategies.backtesting_grip import GridCross
-from strategies.backtesting_mix import MixCross
-from strategies.backtesting_rsi import RSIOscilatorCross
+from strategies.backtesting_rsi import RSIOscillatorCross
 from strategies.backtesting_sma import SmaCross
+from strategies.backtesting_bollinger_bands import BBandsCross
 from strategies.technical_analysis import analyze
-
+from strategies.optimization import optimize
 
 def run_prediction(
     strategy_name, stocks_list, display_dashboard, save_reference, enable_optimizing
@@ -15,26 +11,18 @@ def run_prediction(
     strategies = {
         "SMA": SmaCross,
         "EMA": EmaCross,
-        "RSI": RSIOscilatorCross,
+        "RSI": RSIOscillatorCross,
         "BBands": BBandsCross,
-        "Grid": GridCross,
-        "Mix": MixCross,
         # Add other strategies here
     }
 
     if strategy_name in strategies:
         strategy = strategies[strategy_name]
 
-        args = get_params_defaults(strategy_name)
-        current_optimizer = Optimizer(strategy, stocks_list, args)
         if enable_optimizing:
-            optimized_args = current_optimizer.optimize(
-                get_params_ranges(strategy_name)
-            )
-            args = {
-                key: (range(value, value + 1, 5))
-                for key, value in optimized_args.items()
-            }
-        return analyze(strategy, stocks_list, display_dashboard, save_reference, args)
+            args = optimize(strategy, stocks_list)
+        return analyze(
+            strategy, stocks_list, display_dashboard, save_reference, args
+        )
     else:
         return f"Strategy {strategy_name} not found."
--- strategies/backtesting_rsi.py
+++ strategies/backtesting_rsi.py
@@ -1,21 +1,19 @@
-import talib as ta
-from backtesting import Strategy
 from backtesting.lib import crossover
+import talib as ta
 
+class RSIOscillatorCross:
+    def __init__(self, upper_bound=70, lower_bound=30, rsi_window=14):
+        self.upper_bound = upper_bound
+        self.lower_bound = lower_bound
+        self.rsi_window = rsi_window
 
-class RSIOscilatorCross(Strategy):
-
-    upper_bound = 70
-    lower_bound = 30
-    rsi_window = 14
-
-    def init(self):
-        self.price = self.data.Close
-        self.rsi = self.I(ta.RSI, self.price, self.rsi_window)
-
-    def next(self):
-        # Compare the values
-        if crossover(self.rsi, self.upper_bound):
-            self.position.close()
-        elif crossover(self.lower_bound, self.rsi):
-            self.buy()
+    def generate_signal(self, df):
+        price = df['Close']
+        rsi = ta.RSI(price, self.rsi_window)
+        
+        if crossover(rsi, self.upper_bound):
+            return "UPWARD"
+        elif crossover(self.lower_bound, rsi):
+            return "DOWNWARD"
+        else:
+            return None
